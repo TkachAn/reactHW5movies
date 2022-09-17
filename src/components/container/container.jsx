3ee7fd36f66894c5356efa7a581239abf7868bce
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-17 15:37:31 +0300

    Create container.jsx

---
+++ src/components/container/container.jsx
@@ -0,0 +1,8 @@
+import css from './container.module.css';
+import PropTypes from 'prop-types';
+export const Container = ({ children }) => {
+  return <div className={css.container}>{children}</div>;
+};
+Container.propTypes = {
+  children: PropTypes.node,
+};
