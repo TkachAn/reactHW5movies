d11d1775b16e26e1d1adb612d87b86705c185faf
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-17 18:51:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="">
+    <BrowserRouter basename="/reactHW5movies/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
