c1907e7f2e9cbbbc88ea79a399d3198daddea621
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-17 15:34:53 +0300

    Create header.jsx

---
+++ src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom';
+import css from './header.module.css';
+export const Header = () => {
+  let activeClassName = {
+    color: '#90cea1',
+    fontWeight: 700,
+  };
+
+  return (
+    <header className={css.header}>
+      <nav className={css.navs}>
+        <ul className={css.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={css.links}
+              style={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/search"
+              className={css.links}
+              style={({ isActive }) => (isActive ? activeClassName : undefined)}
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
