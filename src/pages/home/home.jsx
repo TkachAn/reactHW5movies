d683a9961e8a435bcc9bd510d369ae7ed546ddd0
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-17 15:29:17 +0300

    Create home.jsx

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, lazy } from 'react';
+import { apiTMDbTrend } from 'TMDB/API';
+import css from './home.module.css';
+// import { List } from 'components/moviesList/moviesList';
+const List = lazy(() =>
+  import('components/moviesList/moviesList' /* webpackChunkName: "List" */)
+);
+export default function Home() {
+  const [trendMovies, setTrendMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchTMBd = () => {
+      setLoading(true);
+      apiTMDbTrend()
+        .then(res => {
+          setTrendMovies(res);
+        })
+        .catch(error => {
+          setError(`UPS, this is ${error}`);
+        })
+        .finally(setLoading(false));
+    };
+    fetchTMBd();
+  }, []);
+
+  return (
+    <main>
+      <h2 className={css.title}>Trending Movies</h2>
+      {loading && <h3>LOADING...</h3>}
+      {trendMovies && <List movies={trendMovies} />}
+      {error && <div>{('error', error)}</div>}
+    </main>
+  );
+}
